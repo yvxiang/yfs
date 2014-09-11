47c7c96ca6685d307fa227f092fd61ceeb7eb2a8
root <root@ubuntu188.(none)>
2014-09-12 02:55:03 +0800

    Lab4 with no RPCLOOSY

--- lock_client_cache.cc
+++ lock_client_cache.cc
@@ -22,20 +22,90 @@ lock_client_cache::lock_client_cache(std::string xdst,
   std::ostringstream host;
   host << hname << ":" << rlsrpc->port();
   id = host.str();
+
+  pthread_mutex_init(&lock_stat_map_lock, NULL);
+  pthread_cond_init(&acquire_wait_cond, NULL);
 }
 
 lock_protocol::status
 lock_client_cache::acquire(lock_protocol::lockid_t lid)
 {
   int ret = lock_protocol::OK;
-  return lock_protocol::OK;
+  bool have_lock = false, err = false;
+  std::map<lock_protocol::lockid_t, lock_stat>::iterator it;
+  pthread_mutex_lock(&lock_stat_map_lock);
+
+  it = lock_stat_map.find(lid);
+  if(it == lock_stat_map.end()) {
+      it = lock_stat_map.insert(std::make_pair(lid, lock_stat())).first;
+  }
+
+  do {
+      if(it->second.ls == NONE || (it->second.ls == ACQUIRING &&
+                                    it->second.retry == true)
+                                || it->second.ls == RELEASING) {
+          it->second.ls = ACQUIRING;
+          it->second.retry = false;
+
+          pthread_mutex_unlock(&lock_stat_map_lock);
+
+          int r;
+          lock_protocol::status  rpcret = cl->call(lock_protocol::acquire,
+                                            lid, id, r); 
+
+          pthread_mutex_lock(&lock_stat_map_lock);
+
+          if(rpcret == lock_protocol::OK) {
+              it->second.ls = LOCKED;
+              have_lock = true;
+          } else if(rpcret == lock_protocol::RETRY) {
+          } else {
+              err = true;
+              ret = rpcret;
+          }
+      } else if(it->second.ls == FREE) {
+          it->second.ls = LOCKED;
+          have_lock = true;
+      } else if(it->second.ls == LOCKED || (it->second.ls ==
+                    ACQUIRING && it->second.retry == false)) {
+          pthread_cond_wait(&acquire_wait_cond, &lock_stat_map_lock);
+      }
+  } while(!have_lock && !err);
+
+  pthread_mutex_unlock(&lock_stat_map_lock);
+  return ret;
 }
 
 lock_protocol::status
 lock_client_cache::release(lock_protocol::lockid_t lid)
 {
-  return lock_protocol::OK;
+  int ret = lock_protocol::OK;
+  std::map<lock_protocol::lockid_t, lock_stat>::iterator it;
+  pthread_mutex_lock(&lock_stat_map_lock);
 
+  it = lock_stat_map.find(lid);
+  if(it == lock_stat_map.end()) {
+      printf("ERROR try to release a lock that doesn't exist\n");
+      ret = lock_protocol::NOENT;
+  } else {
+      if(it->second.revoke == true) {
+          it->second.revoke = false;
+          it->second.ls = RELEASING;
+
+          pthread_mutex_unlock(&lock_stat_map_lock);
+
+          int r;
+          ret = cl->call(lock_protocol::release, lid, id, r);
+
+          pthread_mutex_lock(&lock_stat_map_lock);
+      } else {
+          it->second.ls = FREE;
+          pthread_cond_broadcast(&acquire_wait_cond);
+      }
+  }
+
+  pthread_mutex_unlock(&lock_stat_map_lock);
+  return ret;
 }
 
 rlock_protocol::status
@@ -43,6 +113,29 @@ lock_client_cache::revoke_handler(lock_protocol::lockid_t lid,
                                   int &)
 {
   int ret = rlock_protocol::OK;
+  std::map<lock_protocol::lockid_t, lock_stat>::iterator it;
+  pthread_mutex_lock(&lock_stat_map_lock);
+
+  it = lock_stat_map.find(lid);
+  if(it == lock_stat_map.end()) {
+      printf("ERROR try to revoke a lock that doesn't exist\n");
+  } else {
+      if(it->second.ls == FREE) {
+          it->second.ls = RELEASING;
+          it->second.revoke = false;
+
+          pthread_mutex_unlock(&lock_stat_map_lock);
+
+          int r;
+          ret = cl->call(lock_protocol::release, lid, id, r);
+
+          pthread_mutex_lock(&lock_stat_map_lock);
+      } else {
+          it->second.revoke = true;
+      }
+  }
+  pthread_mutex_unlock(&lock_stat_map_lock);
+
   return ret;
 }
 
@@ -51,8 +144,18 @@ lock_client_cache::retry_handler(lock_protocol::lockid_t lid,
                                  int &)
 {
   int ret = rlock_protocol::OK;
+  std::map<lock_protocol::lockid_t, lock_stat>::iterator it;
+
+  pthread_mutex_lock(&lock_stat_map_lock);
+
+  it = lock_stat_map.find(lid);
+  if(it == lock_stat_map.end()) {
+      printf("ERROR try to retry a lock that doesn't exist\n");
+  } else {
+      it->second.retry = true;
+      pthread_cond_broadcast(&acquire_wait_cond);
+  }
+
+  pthread_mutex_unlock(&lock_stat_map_lock);
   return ret;
 }
-
-
-
--- lock_client_cache.h
+++ lock_client_cache.h
@@ -4,6 +4,7 @@
 
 #define lock_client_cache_h
 
+#include <set>
 #include <string>
 #include "lock_protocol.h"
 #include "rpc.h"
@@ -20,11 +21,30 @@ class lock_release_user {
 };
 
 class lock_client_cache : public lock_client {
+
+ typedef enum {
+     NONE,
+     FREE,
+     LOCKED,
+     ACQUIRING,
+     RELEASING
+ }LOCK_STAT;
+
+ struct lock_stat {
+     LOCK_STAT ls;
+     bool revoke;
+     bool retry;
+     lock_stat() : ls(NONE), revoke(false), retry(false) {}
+ };
+
  private:
   class lock_release_user *lu;
   int rlock_port;
   std::string hostname;
   std::string id;
+  std::map<lock_protocol::lockid_t, lock_stat> lock_stat_map;
+  pthread_mutex_t lock_stat_map_lock;
+  pthread_cond_t acquire_wait_cond;
  public:
   lock_client_cache(std::string xdst, class lock_release_user *l = 0);
   virtual ~lock_client_cache() {};
